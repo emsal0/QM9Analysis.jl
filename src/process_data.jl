aac4e0a5d29402f3f11dec5bbcfa9bf871c62276
emsal <absolutezero2a03@gmail.com>
2021-10-26 10:07:24 -0700

    Add hydrogen bonds

--- src/process_data.jl
+++ src/process_data.jl
@@ -4,7 +4,7 @@ using JSON
 using GeometricFlux
 using DataFrames
 using LightGraphs
-using JLD2
+using Serialization
 
 function process_atom_data(lines)
     matx = zeros(Float64, 4, length(lines))
@@ -31,6 +31,7 @@ function get_df(filename)
     raw_props = split(raw_lines[2], '\t')
     props_dict = Dict(zip(props_meta, raw_props))
     raw_atom_data = raw_lines[3:3+n_atoms-1]
+    raw_numbers = raw_lines[3+n_atoms]
     smiles = split(raw_lines[4+n_atoms], '\t')
 
     mol_data = RDKitMinimalLib.get_mol(smiles[1]) |> RDKitMinimalLib.get_json |> JSON.parse
@@ -39,17 +40,59 @@ function get_df(filename)
     mol_graph = SimpleGraph(n_atoms)
 
     num_bonds = length(moljson["bonds"])
-    nf = process_atom_data(raw_atom_data)
-    ef = zeros(1, num_bonds)
+    num_non_hydrogen = length(moljson["atoms"])
+
     for (i,bond) in enumerate(moljson["bonds"])
         add_edge!(mol_graph, bond["atoms"][1] + 1,  bond["atoms"][2] + 1)
-        ef[i] = get(bond, "bo", 1)
     end
 
+    Hcount = 0
+    for (i,atom) in enumerate(moljson["atoms"])
+        n_implicit_H = get(atom, "impHs", 0)
+        for _ in 1:n_implicit_H
+            add_edge!(mol_graph, i, num_non_hydrogen + 1 + Hcount)
+            Hcount += 1
+            num_bonds += 1
+        end
+    end
+    @assert Hcount == n_atoms - num_non_hydrogen
+
+    nf = process_atom_data(raw_atom_data)
+    ef = zeros(1, num_bonds)
+    bond_counter = 1
+    for bond in moljson["bonds"]
+        ef[bond_counter] = get(bond, "bo", 1)
+        bond_counter += 1
+    end
+
+    for atom in moljson["atoms"]
+        n_implicit_H = get(atom, "impHs", 0)
+        for _ in 1:n_implicit_H
+            ef[bond_counter] = 1
+            bond_counter += 1
+        end
+    end
     mol_fg = FeaturedGraph(mol_graph, nf=nf, ef=ef)
+
 
     return Dict(
                 props_dict...,
-                "molecule" => mol_fg
+                "molecule" => mol_fg,
+                "rawline" => raw_numbers,
            )
 end
+
+
+datarows = []
+
+DATADIR = "chemdata/"
+for datafile in readdir(DATADIR)
+    try
+       push!(datarows, get_df(DATADIR * datafile))
+    catch
+        println("Error in file $(datafile)")
+    end
+end
+
+@show datarows[1:100]
+serialize("data.blob", datarows)
